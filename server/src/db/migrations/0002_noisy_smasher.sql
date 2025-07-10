d89f34ff1484d2b4901df64ae0ddccf3a8a26ed1
dkat <dkat.dev@gmail.com>
2025-07-10 15:58:27 -0300

    feat: finalizing project

--- server/src/db/migrations/0002_noisy_smasher.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "questions" RENAME COLUMN "roomid" TO "room_id";--> statement-breakpoint
-ALTER TABLE "questions" DROP CONSTRAINT "questions_roomid_rooms_id_fk";
---> statement-breakpoint
-ALTER TABLE "questions" ADD CONSTRAINT "questions_room_id_rooms_id_fk" FOREIGN KEY ("room_id") REFERENCES "public"."rooms"("id") ON DELETE no action ON UPDATE no action;
